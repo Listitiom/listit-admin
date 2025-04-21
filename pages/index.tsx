89d9637ca1208841bcec614a951a199ce3d60d77
Listitiom <Hello@listit.ie>
2025-04-21 17:46:59 +0100

    Delete pages directory

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', padding: '3rem' }}>
-      <h1>Welcome to the Listit Admin Panel</h1>
-      <p>Go to <a href="/admin">/admin</a> to access the dashboard.</p>
-    </div>
-  );
-}
