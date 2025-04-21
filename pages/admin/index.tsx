4a1513caff5a0da3a102812f4827ef44f90340e7
Listitiom <Hello@listit.ie>
2025-04-21 17:59:06 +0100

    Delete pages directory

--- pages/admin/index.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function AdminPanel() {
-  return (
-    <div style={{ textAlign: 'center', padding: '3rem' }}>
-      <h1>Admin Dashboard</h1>
-      <p>This is where you’ll manage images, help pages, and contact content.</p>
-    </div>
-  );
-}
