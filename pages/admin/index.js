e2877ffde4391c498fe4aeff77998efdf6c59065
Listitiom <Hello@listit.ie>
2025-04-22 07:26:08 +0100

    Add files via upload

---
+++ pages/admin/index.js
@@ -0,0 +1,9 @@
+
+export default function AdminPanel() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Super Admin Panel</h1>
+      <p>Use this page to manage your content.</p>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Welcome to Listit Admin</h1>
+      <p>Visit <a href="/admin">/admin</a> to manage your platform.</p>
+    </div>
+  );
+}
